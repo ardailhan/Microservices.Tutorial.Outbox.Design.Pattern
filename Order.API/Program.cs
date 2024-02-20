8455f790826978f04cb12e7f84dea82362ccb34b
Arda İlhan <ardailhan7@gmail.com>
2024-02-20 12:58:53 +0300

    Add project files.

---
+++ Order.API/Program.cs
@@ -0,0 +1,77 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Order.API.Models.Contexts;
+using Order.API.Models.Entities;
+using Order.API.ViewModels;
+using Shared;
+using Shared.Events;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<OrderDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServer")));
+
+var app = builder.Build();
+
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapPost("/create-order", async (CreateOrderVM model, OrderDbContext orderDbContext, ISendEndpointProvider sendEndpointProvider) =>
+{
+    Order.API.Models.Entities.Order order = new()
+    {
+        BuyerId = model.BuyerId,
+        CreatedDate = DateTime.UtcNow,
+        TotalPrice = model.OrderItems.Sum(oi => oi.Count * oi.Price),
+        OrderItems = model.OrderItems.Select(oi => new Order.API.Models.Entities.OrderItem
+        {
+            Price = oi.Price,
+            Count = oi.Count,
+            ProductId = oi.ProductId,
+        }).ToList(),
+    };
+
+    await orderDbContext.Orders.AddAsync(order);
+    await orderDbContext.SaveChangesAsync();
+    OrderCreatedEvent orderCreatedEvent = new()
+    {
+        BuyerId = order.BuyerId,
+        OrderId = order.Id,
+        TotalPrice = model.OrderItems.Sum(oi => oi.Count * oi.Price),
+        OrderItems = model.OrderItems.Select(oi => new Shared.Datas.OrderItem
+        {
+            Price = oi.Price,
+            Count = oi.Count,
+            ProductId = oi.ProductId
+        }).ToList()
+    };
+    #region Outbox Pattern Olmadan
+    //var sendEndpoint = await sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{RabbitMQSettings.Stock_OrderCreatedEvent}"));
+    //await sendEndpoint.Send<OrderCreatedEvent>(orderCreatedEvent);
+    #endregion
+
+    #region Outbox Pattern Calismasi
+    OrderOutbox orderOutbox = new()
+    {
+        OccuredOn = DateTime.UtcNow,
+        ProcessedDate = null,
+        Payload = JsonSerializer.Serialize(orderCreatedEvent),
+        Type = orderCreatedEvent.GetType().Name
+    };
+    await orderDbContext.OrderOutboxes.AddAsync(orderOutbox);
+    await orderDbContext.SaveChangesAsync();
+    #endregion
+
+});
+
+
+app.UseHttpsRedirection();
+
+
+app.Run();
